250266ac3bf8c5d708f0bd209643added3e15959
Aayusha <ayushaghimireg@gmail.com>
2026-05-20 00:47:30 +0545

    Added responsiveness in cutout

--- src/Components/ParallaxCutout.jsx
+++ src/Components/ParallaxCutout.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const ParallaxCutout = () => {
   return (
-    <div className="relative w-full min-h-screen bg-[#fce4ec] flex items-center justify-center px-10 py-20 box-border">
+
+    <div className="relative w-full  bg-[#fce4ec] flex items-center justify-center px-4 md:px-10 lg:px-20 py-10 md:py-20 box-border overflow-hidden">
       <h1 
-        className="select-none text-[40vw] uppercase tracking-tighter leading-none m-0 p-0 text-center w-full block"
+        className="select-none text-[50vw] md:text-[45vw] lg:text-[40vw] uppercase tracking-tighter leading-none m-0 py-10 text-center w-full block"
         style={{ 
           fontFamily: "'Poppins', sans-serif",  
           fontWeight: 400, 
